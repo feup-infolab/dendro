6cdfb761fe5911a7ad6744b77214703dd3d9fb7e
NelsonPereira1991 <nelsonpereira1991@gmail.com>
2016-12-16 12:42:53 +0000

    First experiment with polling notifications with a time interval

--- public/app/js/controllers/notifications/notification_controller.js
+++ public/app/js/controllers/notifications/notification_controller.js
@@ -2,20 +2,43 @@ angular.module('dendroApp.controllers')
     /**
      *  Project administration controller
      */
-    .controller('notificationCtrl', function ($scope, $http, $filter, notificationService, $window, $element)
+    .controller('notificationCtrl', function ($scope, $http, $filter, notificationService, $window, $element, $interval)
     {
         $scope.numNotifications = 0;
         $scope.notifsUris = [];
+        $scope.awaitingResponse = false;
+
+        /*
+        function load_notifications() {
+            if(!$scope.awaitingResponse)
+            {
+                $scope.awaitingResponse = true;
+
+            }
+        }*/
+
         $scope.get_unread_notifications = function()
         {
-            //TODO here put notification polling with time
-            notificationService.getUserUnreadNotifications()
-                .then(function (response) {
-                    $scope.numNotifications = response.data.length;
-                    $scope.notifsUris = response.data;
-                })
-                .catch(function (error) {
-                    console.error("Error getting unread notifications" + JSON.stringify(error));
-                });
-        }
+            if(!$scope.awaitingResponse)
+            {
+                $scope.awaitingResponse = true;
+                notificationService.getUserUnreadNotifications()
+                    .then(function (response) {
+                        $scope.numNotifications = response.data.length;
+                        $scope.notifsUris = response.data;
+                        $scope.awaitingResponse = false;
+                    })
+                    .catch(function (error) {
+                        console.error("Error getting unread notifications" + JSON.stringify(error));
+                        $scope.awaitingResponse = false;
+                    });
+            }
+        };
+
+        $scope.init = function () {
+            $scope.get_unread_notifications();
+            $interval($scope.get_unread_notifications, 60000);
+        };
+
+
     });
