412e484236f6246c24eec8a49a6805f27fd08185
João Rocha da Silva <joaorosilva@gmail.com>
2017-02-28 15:00:28 +0000

    Fixing Analytics token....
    Fixing Analytics token....

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -3,49 +3,49 @@
 //this HAS TO BE A GLOBAL VAR https://github.com/sciactive/pnotify/issues/23
 PNotify.prototype.options.styling = "bootstrap3";
 var stack_topright = {"dir1": "down", "dir2": "left", "push": "top"};
-// Declare app level module which depends on filters, and services
-angular.module('dendroApp', [
-    'ngRoute',
-    'ngAnimate',
-    'ngTagsInput',
-    'ngStorage',
-    'ui.tree',
-    'angular-loading-bar',
-    'ui.bootstrap',
-    'codemwnci.markdown-edit-preview',
-    'angularMoment',
-    'hljs',
-    'ngFileUpload',
-    'ngJSONPath',
-    'TreeWidget',
-    'angularUtils.directives.dirPagination',
-    'ngAlerts',
-    'angular-google-analytics',
-    'dendroApp.controllers',
-    'dendroApp.filters',
-    'dendroApp.services',
-    'dendroApp.directives',
-    'dendroApp.factories',
-]).filter('trustAsResourceUrl', ['$sce', function($sce) {
-    return function(val) {
-        return $sce.trustAsResourceUrl(val);
-    };
-}]).config(['ngAlertsProvider', function (ngAlertsProvider) {
-    // Global empty list text.
-    ngAlertsProvider.options.emptyListText = 'Nothing here...';
 
-    // The queue timeout for new alerts.
-    ngAlertsProvider.options.queue = null;
-}]).config(['AnalyticsProvider', function (AnalyticsProvider) {
-    // Add configuration code as desired
-    $.ajax({
-            url: "/analytics_tracking_code",
-            headers: {"Accept": "application/json"},
-            success: function (analytics_tracking_code)
-            {
-                AnalyticsProvider.setAccount(analytics_tracking_code);  //UU-XXXXXXX-X should be your tracking code
-            }
-        }
-    );
-}]).run(['Analytics', function(Analytics) { }]);
+var bootstrapAngular = function( analytics_tracking_code ) {
+    // Declare app level module which depends on filters, and services
+    angular.module('dendroApp', [
+        'ngRoute',
+        'ngAnimate',
+        'ngTagsInput',
+        'ngStorage',
+        'ui.tree',
+        'angular-loading-bar',
+        'ui.bootstrap',
+        'codemwnci.markdown-edit-preview',
+        'angularMoment',
+        'hljs',
+        'ngFileUpload',
+        'ngJSONPath',
+        'TreeWidget',
+        'angularUtils.directives.dirPagination',
+        'ngAlerts',
+        'angular-google-analytics',
+        'dendroApp.controllers',
+        'dendroApp.filters',
+        'dendroApp.services',
+        'dendroApp.directives',
+        'dendroApp.factories'
+    ]).filter('trustAsResourceUrl', ['$sce', function($sce) {
+        return function(val) {
+            return $sce.trustAsResourceUrl(val);
+        };
+    }]).config(['ngAlertsProvider', function (ngAlertsProvider) {
+        // Global empty list text.
+        ngAlertsProvider.options.emptyListText = 'Nothing here...';
+
+        // The queue timeout for new alerts.
+        ngAlertsProvider.options.queue = null;
+    }]).config(['AnalyticsProvider', function (AnalyticsProvider) {
+        // Add configuration code as desired
+        AnalyticsProvider.setAccount(analytics_tracking_code);  //UU-XXXXXXX-X should be your tracking code
+    }]).run(['Analytics', function(Analytics) { }]);
+};
+
+$.ajax({url : "/analytics_tracking_code",
+    headers: {"Accept": "application/json"},
+    success : bootstrapAngular
+});
 
