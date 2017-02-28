949b9cfd46cfb976854f7d6d2ee8783668f45a29
João Rocha da Silva <joaorosilva@gmail.com>
2017-02-28 13:38:09 +0000

    Updated deprecated dependencies
    Updated deprecated dependencies

--- src/models/social/share.js
+++ src/models/social/share.js
@@ -5,7 +5,7 @@ var Resource = require(Config.absPathInSrcFolder("/models/resource.js")).Resourc
 var Descriptor = require(Config.absPathInSrcFolder("/models/meta/descriptor.js")).Descriptor;
 var Event = require(Config.absPathInSrcFolder("/models/social/event.js")).Event;
 var Post = require(Config.absPathInSrcFolder("/models/social/post.js")).Post;
-var uuid = require('node-uuid');
+var uuid = require('uuid');
 
 var db = function() { return GLOBAL.db.default; }();
 var db_social = function() { return GLOBAL.db.social; }();
