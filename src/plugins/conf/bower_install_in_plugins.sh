51360824eaa222abd8135284a501faa2aae21595
João Rocha da Silva <joaorosilva@gmail.com>
2017-03-02 16:04:56 +0000

    Fixed bower dependencies install script in plugins folder
    Fixed bower dependencies install script in plugins folder

--- src/plugins/conf/bower_install_in_plugins.sh
+++ src/plugins/conf/bower_install_in_plugins.sh
@@ -2,15 +2,17 @@
 
 echo "Running bower install in plugins folders"
 
-start_dir=$(pwd)
+starting_folder=$(pwd)
+plugins_folder=$(pwd)/src/plugins
+echo $plugins_folder
 
-for i in $(find .. | grep bower.json)
+for i in $(find $plugins_folder | grep bower.json)
 do
     folder=${i%/*}
     echo $folder
     cd $folder
     bower install
-    cd $start_dir
+    cd $plugins_folder
 done
 
-cd $start_dir
+cd $starting_folder
