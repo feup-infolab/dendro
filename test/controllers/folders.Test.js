3e1775987a42d9e2cdc08a128ce472ffc82b212c
NelsonPereira1991 <nelsonpereira1991@gmail.com>
2017-03-08 10:54:20 +0000

    Cleaning some code
    Cleaning some code

--- test/controllers/folders.Test.js
+++ test/controllers/folders.Test.js
@@ -1,9 +1,9 @@
 "use strict";
 
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = "test";
 
-let chai = require('chai');
-let chaiHttp = require('chai-http');
+let chai = require("chai");
+let chaiHttp = require("chai-http");
 const md5 = require("md5");
 chai.use(chaiHttp);
 
@@ -15,7 +15,7 @@ const demouser1 = require("../mockdata/users/demouser1.js");
 const demouser2 = require("../mockdata/users/demouser1.js");
 const demouser3 = require("../mockdata/users/demouser1.js");
 
-const folder = require('../mockdata/folders/folder.js');
+const folder = require("../mockdata/folders/folder.js");
 
 const metadataOnlyProject = require("../mockdata/projects/metadata_only_project.js");
 const publicProject = require("../mockdata/projects/public_project.js");
@@ -25,10 +25,16 @@ const fileUtils= require("../utils/file/fileUtils.js");
 const folderUtils= require("../utils/folder/folderUtils.js");
 const userUtils = require("../utils/user/userUtils.js");
 
-describe('/project/' + publicProject.handle + "/data/" + folder.pathInProject + folder.name + "?download", function ()
+describe("/project/" + publicProject.handle + "/data/" + folder.pathInProject + folder.name + "?download", function ()
 {
-    it('should download folder from a public project, user unauthenticated', function (done) {
-                if (err) done(err);
+    it("should download folder from a public project, user unauthenticated", function (done) {
+        let app = GLOBAL.tests.app;
+        agent = chai.request.agent(app);
+        folderUtils.downloadFolder(false, agent, folder.pathInProject, folder.name, publicProject.handle, function (err, res) {
+                if (err)
+                {
+                    done(err);
+                }
 
                 res.should.have.status(200);
                 md5(res.body).should.be.equal.to(folder.download_md5);
@@ -36,9 +42,13 @@ describe('/project/' + publicProject.handle + "/data/" + folder.pathInProject + 
             });
     });
 
-    it('should download folder from a public project, user authenticated as ' + demouser1 + " (creator) ", function (done) {
+    it("should download folder from a public project, user authenticated as " + demouser1 + " (creator) ", function (done) {
         userUtils.loginUser(demouser1.username, demouser1.password, function (err, agent) {
-                if (err) done(err);
+            folderUtils.downloadFolder(false, agent, folder.pathInProject, folder.name, publicProject.handle, function (err, res) {
+                if (err)
+                {
+                    done(err);
+                }
 
                 res.should.have.status(200);
                 md5(res.body).should.be.equal.to(folder.download_md5);
@@ -46,10 +56,13 @@ describe('/project/' + publicProject.handle + "/data/" + folder.pathInProject + 
             });
         });
     });
-    
-    it('should download folder from a public project, user authenticated as ' + demouser2.username + " (contributor) ", function (done) {
+    it("should download folder from a public project, user authenticated as " + demouser2.username + " (contributor) ", function (done) {
         userUtils.loginUser(demouser2.username, demouser2.password, function (err, agent) {
-                if (err) done(err);
+            folderUtils.downloadFolder(false, agent, folder.pathInProject, folder.name, publicProject.handle, function (err, res) {
+                if (err)
+                {
+                    done(err);
+                }
 
                 res.should.have.status(200);
                 md5(res.body).should.be.equal.to(folder.download_md5);
@@ -58,9 +71,13 @@ describe('/project/' + publicProject.handle + "/data/" + folder.pathInProject + 
         });
     });
 
-    it('should download folder from a public project, user authenticated as ' + demouser3.username + " (not related to the project) ", function (done) {
+    it("should download folder from a public project, user authenticated as " + demouser3.username + " (not related to the project) ", function (done) {
         userUtils.loginUser(demouser3.username, demouser3.password, function (err, agent) {
-                if (err) done(err);
+            folderUtils.downloadFolder(false, agent, folder.pathInProject, folder.name, publicProject.handle, function (err, res) {
+                if (err)
+                {
+                    done(err);
+                }
 
                 res.should.have.status(200);
                 md5(res.body).should.be.equal.to(folder.download_md5);
--- test/index.js
+++ test/index.js
@@ -1,21 +1,21 @@
 GLOBAL.tests = {};
 
-require('./homepage.Test.js');
+require("./homepage.Test.js");
 
 //test login
-require('./controllers/auth.Test.js');
+require("./controllers/auth.Test.js");
 
 //test projects
-require('./controllers/projects.Test.js');
+require("./controllers/projects.Test.js");
 
 //test file uploads
-require('./controllers/files.Test.js');
+require("./controllers/files.Test.js");
 
 //test folders
-require('./controllers/folders.Test.js');
+require("./controllers/folders.Test.js");
 
 //test users
-require('./controllers/users.Test.js');
+require("./controllers/users.Test.js");
 
 //destroy graphs
 //require('./models/kb/db.Test.js');
