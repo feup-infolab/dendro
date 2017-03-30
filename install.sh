bd9c6e1636762919ea137a735791edba76253561
João Rocha da Silva <joaorosilva@gmail.com>
2017-03-30 18:03:54 +0100

    Added install script file

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#install NVM, Node 6.10, Node Automatic Version switcher
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash &&
+export NVM_DIR="$HOME/.nvm" &&
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Want NVM to be loaded on every terminal you open? Add to ~/.bash_profile this:
+
+#export NVM_DIR="$HOME/.nvm" &&
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install 6.10
+npm install -g avn avn-nvm avn-n
+avn setup
+
+#install dependencies. Will also run bower install whenever needed
+npm install #this is needed when running npm install with sudo to install global modules
+grunt #use grunt to put everything in place
