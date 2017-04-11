2f22c49dafc215ae2e2267346ff2947c35dab1f5
NelsonPereira1991 <nelsonpereira1991@gmail.com>
2017-04-11 12:28:47 +0100

    Added login calls as the creator
    Added login calls as the creator before adding a contributor to the
    project

--- test_new_structure/units/projects/addContributorsToProjects.Unit.js
+++ test_new_structure/units/projects/addContributorsToProjects.Unit.js
@@ -17,39 +17,38 @@ const demouser2 = require(Config.absPathInTestsFolder("mockdata/users/demouser2"
 require(Config.absPathInTestsFolder("units/projects/createProjects.Unit.js")).setup();
 module.exports.setup = function(finish)
 {
-    /*
-    before(function(){
-        require(Config.absPathInTestsFolder("units/projects/createProjects.Unit.js"));
-    });*/
     describe("[UNIT] add demouser2 as contributor to all projects", function () {
-
-        /*
-        before(function(done){
-            require(Config.absPathInTestsFolder("units/projects/createProjects.Unit.js")).setup(done);
-            done();
-        });*/
 
         it('should add demouser2 as contributor of the public project', function (done) {
             const projectData = require(Config.absPathInTestsFolder("mockdata/projects/public_project.js"));
-            userUtils.addUserAscontributorToProject(true, agent, demouser2.username, projectData.handle, function (err, res) {
-                res.statusCode.should.equal(200);
-                done();
+            userUtils.loginUser(demouser1.username, demouser1.password, function (err, agent) {
+                should.not.exist(err);
+                userUtils.addUserAscontributorToProject(true, agent, demouser2.username, projectData.handle, function (err, res) {
+                    res.statusCode.should.equal(200);
+                    done();
+                });
             });
         });
 
         it('should add demouser2 as contributor of the private project', function (done) {
             const projectData = require(Config.absPathInTestsFolder("mockdata/projects/private_project.js"));
-            userUtils.addUserAscontributorToProject(true, agent, demouser2.username, projectData.handle, function (err, res) {
-                res.statusCode.should.equal(200);
-                done();
+            userUtils.loginUser(demouser1.username, demouser1.password, function (err, agent) {
+                should.not.exist(err);
+                userUtils.addUserAscontributorToProject(true, agent, demouser2.username, projectData.handle, function (err, res) {
+                    res.statusCode.should.equal(200);
+                    done();
+                });
             });
         });
 
         it('should add demouser2 as contributor of the metadata-only project', function (done) {
             const projectData = require(Config.absPathInTestsFolder("mockdata/projects/metadata_only_project.js"));
-            userUtils.addUserAscontributorToProject(true, agent, demouser2.username, projectData.handle, function (err, res) {
-                res.statusCode.should.equal(200);
-                done();
+            userUtils.loginUser(demouser1.username, demouser1.password, function (err, agent) {
+                should.not.exist(err);
+                userUtils.addUserAscontributorToProject(true, agent, demouser2.username, projectData.handle, function (err, res) {
+                    res.statusCode.should.equal(200);
+                    done();
+                });
             });
         });
 
@@ -59,4 +58,3 @@ module.exports.setup = function(finish)
         });
     });
 };
-
