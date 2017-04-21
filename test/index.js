2d157b4567e1f13f9b9ec6f6aad3fba3064a8a99
João Rocha da Silva <joaorosilva@gmail.com>
2017-04-21 10:46:44 +0100

    Merged master into the branch, resolved conflicts
    Merged master into the branch.

--- test/index.js
+++ test/index.js
@@ -5,28 +5,28 @@ Config.initGlobals();
 
 GLOBAL.tests = {};
 
-require("./homepage.Test.js");
-
-require("./test/test_new_structure/routes/project/public_project/__mkdir/routes.project.publicProject.__mkdir.Test.js");
+require(Config.absPathInTestsFolder("routes/project/public_project/__mkdir/routes.project.publicProject.__mkdir.Test.js"));
 
 /*
 //test login
-//require("./controllers/auth.Test.js");
-
+require("./controllers/auth.Test.js");
 //test projects
 require("./controllers/projects.Test.js");
 
+//test project file operations
+require("./controllers/project.fileOperations/administer.Test.js");
+
 //test file uploads
-//require("./controllers/files.Test.js");
+require("./controllers/files.Test.js");
 
 //test folders
 require("./controllers/folders.Test.js");
 
 //test users
-//require("./controllers/users.Test.js");
+require("./controllers/users.Test.js");
 
 //test descriptors
-//require("./controllers/descriptors.Test.js");
+require("./controllers/descriptors.Test.js");
 
 //SOCIAL DENDRO
 //test social Dendro Posts
