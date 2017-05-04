ad1c3b02f214fd66ab43be130cb983e69b8eba75
williamnf <wnfukunaga@gmail.com>
2017-05-04 18:44:38 +0100

    horseman giving trouble

--- test_new_structure/routes/project/public_project/__upload/routes.project.publicProject.__uploadTest.js
+++ test_new_structure/routes/project/public_project/__upload/routes.project.publicProject.__uploadTest.js
@@ -9,6 +9,7 @@ const async = require('async');
 const Browser = require('zombie');
 const Config = GLOBAL.Config;
 
+
 //let File = require(Config.absPathInSrcFolder('models/directory_structure/file.js')).File;
 
 
@@ -34,46 +35,13 @@ const folder = require(Config.absPathInTestsFolder("mockdata/folders/folder.js")
 var db = appUtils.requireUncached(Config.absPathInTestsFolder("utils/db/db.Test.js"));
 var createFoldersUnit = appUtils.requireUncached(Config.absPathInTestsFolder("units/folders/createFolders.Unit.js"));
 
+
 describe("Upload data projects", function (done) {
     before(function (done) {
         this.timeout(60000);
         createFoldersUnit.setup(function (err, res) {
             should.equal(err, null);
-
-            const phantom = require('phantom');
-            var sitepage = null;
-            var phInstance = null;
-            phantom.create()
-                .then(instance => {
-                    phInstance = instance;
-                    return instance.createPage();
-                })
-                .then(page => {
-
-                    page.on("onResourceRequested", function(requestData) {
-                        page.open('https://stackoverflow.com/', function(err){
-                            console.log(err);
-
-                            page.property('content', function(err, content){
-                                console.log(content);
-                                console.log(err);
-                                done();
-                            });
-                        });
-                    });
-
-
-
-
-
-                })
-                .catch(error => {
-                    console.log(error);
-                    phInstance.exit();
-                });
-
-
-
+            done();
         });
     });
 
@@ -127,14 +95,93 @@ describe("Upload data projects", function (done) {
         it("[HTML] should upload file in folder as creator POST", function (done) {
             userUtils.loginUser(demouser1.username, demouser1.password, function (err, agent) {
 
+                var Horseman = require('node-horseman');
+                var horseman = new Horseman();
+                var phantom = require("phantom");
+
+                horseman
+                    .userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
+                    .open("http://127.0.0.1:3001" + "/login")
+                    .waitForSelector("input[name='password']")
+                    .type('[name="username"]', 'demouser1')
+                    .type('[name="password"]', 'demouserpassword2015')
+                    .value('[name="username"]', 'demouser1')
+                    .value('[name="password"]', 'demouserpassword2015')
+                    .click('button[name="signin"]')
+                    .keyboardEvent('keypress', 16777221)
+                    .waitForNextPage()
+                    .plainText()
+                    /*.open(Config.baseUri + "/project/privateprojectcreatedbydemouser1")
+                    .waitForSelector('#wrap')
+                    .plainText()*/
+                    .log()
+                    .close();
+
+
+                /*horseman
+                    .userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
+                    .open('http://www.google.com')
+                    .type('input[name="q"]', 'github')
+                    .click('[name="btnK"]')
+                    .keyboardEvent('keypress', 16777221)
+                    .waitForSelector('div.g')
+                    .count('div.g')
+                    .log() // prints out the number of results
+                    .close();*/
+
+
+                //done();
+
+                /*phantom.casperTest = true;
+                phantom.casperPath = Config.absPathInApp('node_modules/casperjs');
+                phantom.injectJs(Config.absPathInApp('node_modules/casperjs/bin/bootstrap.js'));
+
+                var casper = require('casper').create();
+                casper.start(Config.baseUri + "/login");
+
+                casper.then(function(){
+                    this.fill('form', {
+                        'username':    demouser1.username,
+                        'password':    demouser1.password
+                    }, true);
+                    this.evaluate(function(){
+                        //trigger click event on submit button
+                        document.querySelector('input[type="submit"]').click();
+                        casper.start(Config.baseUri + "/project/privateprojectcreatedbydemouser1", function () {
+                            var data = this.getHTML();
+                            var ola;
+                            done();
+                        });
+                    });
+                })
+                var _ph, _page, _outObj;
+
+                phantom.create().then(ph => {
+                    _ph = ph;
+                    return _ph.createPage();
+                }).then(page => {
+                    _page = page;
+                    return _page.open(Config.baseUri + "/project/privateprojectcreatedbydemouser1");
+                }).then(status => {
+                    console.log(status);
+                    return _page.property('content')
+                }).then(content => {
+                    console.log(content);
+                    _page.close();
+                    _ph.exit();
+                }).catch(e => console.log(e));*/
+
+
+
+
                 //browser.visit("/projects/my", function(err){
-                    var query = '&filename=all.ejs&size=3549&username=' + demouser1.username;
-                    projectUtils.upload(agent, false, '/data/pastinhaLinda', publicProject.handle, query, function (err, res) {
+                    //var query = '&filename=all.ejs&size=3549&username=' + demouser1.username;
+                    /*projectUtils.upload(agent, false, '/data/pastinhaLinda', publicProject.handle, query, function (err, res) {
                         res.should.have.status(500);
                         should.exist(err);
                         res.text.should.equal('{"result":"error","message":"Upload ID not recognized. Please restart uploading undefinedfrom the beginning."}');
                         done();
-                    });
+                    });*/
                 //});
             });
         });
