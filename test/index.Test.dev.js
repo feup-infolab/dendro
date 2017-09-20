093e3188071a635d9a21cfcdaf2f4574b6b5e153
NelsonPereira1991 <nelsonpereira1991@gmail.com>
2017-09-20 16:07:51 +0100

    done stubs for /notifications/all
    done stubs for /notifications/all

--- test/index.Test.dev.js
+++ test/index.Test.dev.js
@@ -32,7 +32,7 @@ require(Pathfinder.absPathInTestsFolder("/routes/archived_resource/routes.archiv
 '/posts/countNum' -> done route and file and stubs
 '/posts/:uri' -> done route and file and stubs
 '/shares/:uri' -> done route and file and stubs
-'/notifications/all' -> done route and file
+'/notifications/all' -> done route and file and stubs
 '/notifications/notification' -> done route and file
 '/notifications/notification' -> done route and file
 */
