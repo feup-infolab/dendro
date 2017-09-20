4bd01df129eee7fa93d56f225f59df48f79cb194
NelsonPereira1991 <nelsonpereira1991@gmail.com>
2017-09-20 11:39:19 +0100

    Done stubs for the 'socialDendro/my' route
    Done stubs for the 'socialDendro/my' route

--- test/routes/socialDendro/my/routes.socialDendro.my.Test.js
+++ test/routes/socialDendro/my/routes.socialDendro.my.Test.js
@@ -1,3 +1,73 @@
-/**
- * Created by Utilizador on 19/09/2017.
- */
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const should = chai.should();
+const _ = require("underscore");
+const md5 = require("md5");
+const fs = require("fs");
+const path = require("path");
+const async = require("async");
+chai.use(chaiHttp);
+
+const Pathfinder = global.Pathfinder;
+const Config = require(Pathfinder.absPathInSrcFolder("models/meta/config.js")).Config;
+
+const userUtils = require(Pathfinder.absPathInTestsFolder("utils/user/userUtils.js"));
+const fileUtils = require(Pathfinder.absPathInTestsFolder("utils/file/fileUtils.js"));
+const itemUtils = require(Pathfinder.absPathInTestsFolder("utils/item/itemUtils.js"));
+const appUtils = require(Pathfinder.absPathInTestsFolder("utils/app/appUtils.js"));
+const projectUtils = require(Pathfinder.absPathInTestsFolder("utils/project/projectUtils.js"));
+const versionUtils = require(Pathfinder.absPathInTestsFolder("utils/versions/versionUtils.js"));
+const descriptorUtils = require(Pathfinder.absPathInTestsFolder("utils/descriptor/descriptorUtils.js"));
+
+const demouser1 = require(Pathfinder.absPathInTestsFolder("mockdata/users/demouser1.js"));
+const demouser2 = require(Pathfinder.absPathInTestsFolder("mockdata/users/demouser2.js"));
+const demouser3 = require(Pathfinder.absPathInTestsFolder("mockdata/users/demouser3.js"));
+
+const privateProject = require(Pathfinder.absPathInTestsFolder("mockdata/projects/private_project.js"));
+const invalidProject = require(Pathfinder.absPathInTestsFolder("mockdata/projects/invalidProject.js"));
+
+const testFolder1 = require(Pathfinder.absPathInTestsFolder("mockdata/folders/testFolder1.js"));
+const notFoundFolder = require(Pathfinder.absPathInTestsFolder("mockdata/folders/notFoundFolder.js"));
+const folderForDemouser2 = require(Pathfinder.absPathInTestsFolder("mockdata/folders/folderDemoUser2"));
+const createFoldersUnit = appUtils.requireUncached(Pathfinder.absPathInTestsFolder("units/folders/createFolders.Unit.js"));
+const db = appUtils.requireUncached(Pathfinder.absPathInTestsFolder("utils/db/db.Test.js"));
+
+describe("Social Dendro user timeline tests", function () {
+    before(function (done) {
+        this.timeout(60000);
+        //creates posts for the 3 types of projects(public, private, metadataOnly)
+        createSocialDendroTimelineWithPostsAndShares.setup(function (err, results) {
+            should.equal(err, null);
+            done();
+        });
+    });
+
+    describe("[GET] Gets the Social Dendro timeline for each user [Valid cases] /socialDendro/my", function () {
+
+        it("[For an unauthenticated user] Should give an unauthorized error", function (done) {
+            done();
+        })
+
+        it("[For demouser1, as the creator of all projects] Should give 'a' number of metadataChangePosts, 'b' number of fileSystemPosts, 'c' number of manualPosts and 'd' number of shares", function (done) {
+            done();
+        })
+
+        it("[For demouser2, a collaborator in all projects] Should give 'a' number of metadataChangePosts, 'b' number of fileSystemPosts, 'c' number of manualPosts and 'd' number of shares", function (done) {
+            done();
+        })
+
+        it("[For demouser3, is not a creator or collaborator in any projects] the timeline should be empty", function (done) {
+            done();
+        })
+    });
+
+    after(function (done) {
+        //destroy graphs
+        this.timeout(Config.testsTimeout);
+        appUtils.clearAppState(function (err, data) {
+            should.equal(err, null);
+            done();
+        });
+    });
+
+})
