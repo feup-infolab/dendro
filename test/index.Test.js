0c113fa3caeb9bea2454606f0176f416e1d1e1a5
NelsonPereira1991 <nelsonpereira1991@gmail.com>
2017-09-29 11:14:16 +0100

    Updated the main index.Test file
    Updated the main index.Test file

--- test/index.Test.js
+++ test/index.Test.js
@@ -15,6 +15,44 @@ global.Config = Config;
 
 global.tests = {};
 
+//SOCIAL DENDRO TESTS
+
+require(Pathfinder.absPathInTestsFolder("/routes/socialDendro/my/routes.socialDendro.my.Test.js"));
+
+require(Pathfinder.absPathInTestsFolder("/routes/posts/all/routes.posts.all.Test.js"));
+
+require(Pathfinder.absPathInTestsFolder("/routes/posts/post/routes.posts.post.Test.js"));
+
+require(Pathfinder.absPathInTestsFolder("/routes/posts/posts/routes.posts.posts.Test.js"));
+
+require(Pathfinder.absPathInTestsFolder("/routes/posts/new/routes.posts.new.Test.js"));
+
+require(Pathfinder.absPathInTestsFolder("/routes/posts/like/routes.posts.like.Test.js"));
+
+require(Pathfinder.absPathInTestsFolder("/routes/posts/post/likes/routes.posts.post.likes.Test.js"));
+
+require(Pathfinder.absPathInTestsFolder("/routes/posts/comment/routes.posts.comment.Test.js"));
+
+require(Pathfinder.absPathInTestsFolder("/routes/posts/comments/routes.posts.comments.Test.js"));
+
+require(Pathfinder.absPathInTestsFolder("/routes/posts/share/routes.posts.share.Test.js"));
+
+require(Pathfinder.absPathInTestsFolder("/routes/posts/shares/routes.posts.shares.Test.js"));
+
+require(Pathfinder.absPathInTestsFolder("/routes/posts/count/routes.posts.count.Test.js"));
+
+require(Pathfinder.absPathInTestsFolder("/routes/posts/_uri/routes.posts._uri.Test.js"));
+
+require(Pathfinder.absPathInTestsFolder("/routes/shares/_uri/routes.shares._uri.Test.js"));
+
+require(Pathfinder.absPathInTestsFolder("/routes/notifications/all/routes.notifications.all.Test.js"));
+
+require(Pathfinder.absPathInTestsFolder("/routes/notifications/notification/routes.notifications.notification.Test.js"));
+
+require(Pathfinder.absPathInTestsFolder("/routes/notifications/notification/routes.notifications.notification[DELETE].Test.js"));
+
+//END OF SOCIAL DENDRO TESTS
+
 //require(Pathfinder.absPathInTestsFolder("/routes/archived_resource/routes.archivedResource.Test.js"));
 
 require(Pathfinder.absPathInTestsFolder("/routes/archived_resource/routes.archivedResource.Test.js"));
