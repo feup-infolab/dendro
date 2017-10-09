b640259ad8f4f5d532a69a5cc1ac4dfa1f5055b8
NelsonPereira1991 <nelsonpereira1991@gmail.com>
2017-10-09 16:27:10 +0100

    All tests passing
    All tests passing

--- test/index.Test.dev.js
+++ test/index.Test.dev.js
@@ -25,7 +25,13 @@ require(Pathfinder.absPathInTestsFolder("/init/loadOntologiesCache.Test.js"));
 /*require(Pathfinder.absPathInTestsFolder("/routes/project/private_project/data/folderExportCkan/__calculate_ckan_repository_diffs/routes.project.privateProject.data.folderExportCkan.__calculate_ckan_repository_diffs.Test.js"));*/
 
 //PUBLIC PROJECT FOLDER LEVEL EXPORT TO CKAN TESTS
-require(Pathfinder.absPathInTestsFolder("/routes/project/public_project/data/folderExportCkan/__export_to_repository/routes.project.publicProject.data.folderExportCkan.__export_to_repository[CKAN].Test.js"));
+/*require(Pathfinder.absPathInTestsFolder("/routes/project/public_project/data/folderExportCkan/__export_to_repository/routes.project.publicProject.data.folderExportCkan.__export_to_repository[CKAN].Test.js"));*/
+
+//METADATA ONLY PROJECT FOLDER LEVEL EXPORT TO CKAN TESTS
+require(Pathfinder.absPathInTestsFolder("/routes/project/metadata_only_project/data/folderExportCkan/__export_to_repository/routes.project.metadataOnlyProject.data.folderExportCkan.__export_to_repository[CKAN].Test.js"));
+
+//PRIVATE PROJECT FOLDER LEVEL EXPORT TO CKAN TESTS
+require(Pathfinder.absPathInTestsFolder("/routes/project/private_project/data/folderExportCkan/__export_to_repository/routes.project.privateProject.data.folderExportCkan.__export_to_repository[CKAN].Test.js"));
 
 return;
 
