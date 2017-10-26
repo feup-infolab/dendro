3048ef152f8e09af7c3d7068d555ba8e41c47ae9
NelsonPereira1991 <nelsonpereira1991@gmail.com>
2017-10-26 11:08:12 +0100

    Merged with machine idendifiers still need to run tests to check if everything is ok
    Merged with machine idendifiers still need to run tests to check if
    everything is ok

--- test/index.Test.dev.js
+++ test/index.Test.dev.js
@@ -16,10 +16,30 @@ global.tests = {};
 
 require(Pathfinder.absPathInTestsFolder("/init/loadOntologiesCache.Test.js"));
 
-//PROJECT CHANGES PUBLIC PROJECT
+/*//PROJECT CHANGES PUBLIC PROJECT
 require(Pathfinder.absPathInTestsFolder("/routes/project/public_project/__recent_changes/routes.project.publicProject.__recent_changes.Test.js"));
 //PROJECT CHANGES PRIVATE PROJECT
 require(Pathfinder.absPathInTestsFolder("/routes/project/private_project/__recent_changes/routes.project.privateProject.__recent_changes.Test.js"));
 //PROJECT CHANGES METADADATA ONlY PROJECT
 require(Pathfinder.absPathInTestsFolder("/routes/project/metadata_only_project/__recent_changes/routes.project.metadataonlyProject.__recent_changes.Test.js"));
+return;*/
+
+//PUBLIC PROJECT FOLDER LEVEL CALCULATE CKAN REPOSITORY DIFFS
+require(Pathfinder.absPathInTestsFolder("/routes/project/public_project/data/folderExportCkan/__calculate_ckan_repository_diffs/routes.project.publicProject.data.folderExportCkan.__calculate_ckan_repository_diffs.Test.js"));
+
+//METADATA PROJECT FOLDER LEVEL CALCULATE CKAN REPOSITORY DIFFS
+require(Pathfinder.absPathInTestsFolder("/routes/project/metadata_only_project/data/folderExportCkan/__calculate_ckan_repository_diffs/routes.project.metadataOnlyProject.data.folderExportCkan.__calculate_ckan_repository_diffs.Test.js"));
+
+//PRIVATE PROJECT FOLDER LEVEL CALCULATE CKAN REPOSITORY DIFFS
+require(Pathfinder.absPathInTestsFolder("/routes/project/private_project/data/folderExportCkan/__calculate_ckan_repository_diffs/routes.project.privateProject.data.folderExportCkan.__calculate_ckan_repository_diffs.Test.js"));
+
+//PUBLIC PROJECT FOLDER LEVEL EXPORT TO CKAN TESTS
+require(Pathfinder.absPathInTestsFolder("/routes/project/public_project/data/folderExportCkan/__export_to_repository/routes.project.publicProject.data.folderExportCkan.__export_to_repository[CKAN].Test.js"));
+
+//METADATA ONLY PROJECT FOLDER LEVEL EXPORT TO CKAN TESTS
+require(Pathfinder.absPathInTestsFolder("/routes/project/metadata_only_project/data/folderExportCkan/__export_to_repository/routes.project.metadataOnlyProject.data.folderExportCkan.__export_to_repository[CKAN].Test.js"));
+
+//PRIVATE PROJECT FOLDER LEVEL EXPORT TO CKAN TESTS
+require(Pathfinder.absPathInTestsFolder("/routes/project/private_project/data/folderExportCkan/__export_to_repository/routes.project.privateProject.data.folderExportCkan.__export_to_repository[CKAN].Test.js"));
+
 return;
