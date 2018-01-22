5fed9a58ae895398dfe9ec4d3403bd21aea16c8d
João Rocha <joaorosilva@gmail.com>
2018-01-22 12:21:49 +0000

    Reduced number of simultaneous connections to the database to 1 Updated mocha to 5.0.0

--- test/routes/project/metadata_only_project/data/testFolder2/__recent_changes/routes.project.metadataonlyProject.data.testFolder2.__recent_changes.Test.js
+++ test/routes/project/metadata_only_project/data/testFolder2/__recent_changes/routes.project.metadataonlyProject.data.testFolder2.__recent_changes.Test.js
@@ -145,7 +145,7 @@ describe("Metadata only project testFolder2 level recent changes", function ()
         appUtils.clearAppState(function (err, data)
         {
             should.equal(err, null);
-            done(err);
+            done();
         });
     });
 });
