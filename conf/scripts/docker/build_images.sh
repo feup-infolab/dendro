211b1aab23cc8daa3c7da0de28821cdeb877b502
João Rocha <joaorosilva@gmail.com>
2018-02-01 19:28:02 +0000

    Made scripts executable

--- conf/scripts/docker/build_images.sh
+++ conf/scripts/docker/build_images.sh
@@ -2,6 +2,7 @@
 
 SCRIPTS_DIR="$(pwd)/conf/scripts"
 DOCKER_SCRIPTS_DIR="$(pwd)/conf/scripts/docker"
+DOCKERFILES_DIR="$(pwd)/conf/dockerfiles"
 
 # create data directories to mount data folders in containers
 mkdir -p data/virtuoso
@@ -16,20 +17,20 @@ mkdir -p data/redis-notifications
 
 docker build \
     -t virtuoso:7.2.2-dendro-v0.3 \
-    "$DOCKER_SCRIPTS_DIR/dockerfiles/virtuoso_with_criu_and_ontologies"
+    "$DOCKERFILES_DIR/virtuoso_with_criu_and_ontologies"
 
 docker build \
     -t elasticsearch:2.3.3-dendro-v0.3 \
-    "$DOCKER_SCRIPTS_DIR/dockerfiles/elasticsearch_with_criu"
+    "$DOCKERFILES_DIR/dockerfiles/elasticsearch_with_criu"
 
 docker build \
     -t mysql:8.0.3-dendro-v0.3 \
-    "$DOCKER_SCRIPTS_DIR/dockerfiles/mysql_with_criu"
+    "$DOCKERFILES_DIR/dockerfiles/mysql_with_criu"
 
 docker build \
     -t mongo:3.4.10-dendro-v0.3 \
-    "$DOCKER_SCRIPTS_DIR/dockerfiles/mongo_with_criu"
+    "$DOCKERFILES_DIR/dockerfiles/mongo_with_criu"
 
 docker build -t redis:3.2.11-dendro-v0.3 \
-    "$DOCKER_SCRIPTS_DIR/dockerfiles/redis_with_criu"
+    "$DOCKERFILES_DIR/dockerfiles/redis_with_criu"
 
