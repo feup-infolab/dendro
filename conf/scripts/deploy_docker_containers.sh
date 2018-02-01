56e5bc7641f84b23c66d2059661e3d063ea8d81f
João Rocha <joaorosilva@gmail.com>
2018-02-01 19:06:18 +0000

    Added volume mount pointsCleaned up the code

--- conf/scripts/deploy_docker_containers.sh
+++ conf/scripts/deploy_docker_containers.sh
@@ -36,37 +36,38 @@ docker run --name virtuoso-dendro \
     -p 8890:8890 \
     -p 1111:1111 \
     -e SPARQL_UPDATE=true \
+    -v $(pwd)/data/virtuoso:/data \
     -d virtuoso:7.2.2-dendro-v0.3 \
-    -v $(pwd)/data/virtuoso:/data
 
 
 docker run --name elasticsearch-dendro \
     -p 9200:9200 \
+    -v $(pwd)/data/elasticsearch:/usr/share/elasticsearch \
     -d elasticsearch:2.3.3-dendro-v0.3 \
-    -v $(pwd)/data/elasticsearch:/usr/share/elasticsearch
 
 docker run --name mysql-dendro \
     -p 3306:3306 \
+    -v $(pwd)/data/mysql:/var/lib/mysql \
     -d mysql:8.0.3-dendro-v0.3 \
-    -v $(pwd)/data/mysql:/var/lib/mysql
+
 
 docker run --name mongo-dendro \
     -p 27017:27017 \
-    -d mongo:3.4.10-dendro-v0.3 \
-    -v $(pwd)/data/mongo:/data/db
+    -v $(pwd)/data/mongo:/data/db \
+    -d mongo:3.4.10-dendro-v0.3
 
 
 docker run --name redis-dendro-default \
     -p 6781:6780 \
-    -d redis:3.2.11-dendro-v0.3 \
-    -v $(pwd)/data/redis-default:/data
+    -v $(pwd)/data/redis-default:/data \
+    -d redis:3.2.11-dendro-v0.3
 
 docker run --name redis-dendro-social \
     -p 6782:6780 \
-    -d redis:3.2.11-dendro-v0.3 \
-    -v $(pwd)/data/redis-default:/data
+    -v $(pwd)/data/redis-default:/data \
+    -d redis:3.2.11-dendro-v0.3
 
 docker run --name redis-dendro-notifications \
     -p 6783:6780 \
-    -d redis:3.2.11-dendro-v0.3 \
-    -v $(pwd)/data/redis-default:/data
+    -v $(pwd)/data/redis-default:/data \
+    -d redis:3.2.11-dendro-v0.3
