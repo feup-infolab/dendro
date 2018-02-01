7f52866b9defd6e7c21def47282752549c82c54b
João Rocha <joaorosilva@gmail.com>
2018-02-01 19:01:33 +0000

    Cleaned up the code

--- conf/scripts/deploy_docker_containers.sh
+++ conf/scripts/deploy_docker_containers.sh
@@ -1,29 +1,73 @@
 #!/usr/bin/env bash
 
-#install and boot containers
-docker build -t virtuoso:7.2.2-dendro-v0.3 ../dockerfiles/virtuoso_with_criu_and_ontologies
+# create data directories to mount data folders in containers
 mkdir -p ./data/virtuoso
-docker run --name virtuoso-dendro -p 8890:8890 -p 1111:1111 -e SPARQL_UPDATE=true -d virtuoso:7.2.2-dendro-v0.3 -v ./data/virtuoso:/data
-
-docker build -t elasticsearch:2.3.3-dendro-v0.3 ../dockerfiles/elasticsearch_with_criu
 mkdir -p ./data/elasticsearch
-docker run --name elasticsearch-dendro -p 9200:9200 -d elasticsearch:2.3.3-dendro-v0.3 -v ./data/elasticsearch:/usr/share/elasticsearch
-
-docker build -t mysql:8.0.3-dendro-v0.3 ../dockerfiles/mysql_with_criu
 mkdir -p ./data/mysql
-docker run --name mysql-dendro -p 3306:3306 -d mysql:8.0.3-dendro-v0.3 -v ./data/mysql:/var/lib/mysql
-
-docker build -t mongo:3.4.10-dendro-v0.3 ../dockerfiles/mongo_with_criu
 mkdir -p ./data/mongo
-docker run --name mongo-dendro -p 27017:27017 -d mongo:3.4.10-dendro-v0.3 -v ./data/mongo:/data/db
-
-docker build -t redis:3.2.11-dendro-v0.3 ../dockerfiles/redis_with_criu
-
 mkdir -p ./data/redis-default
-docker run --name redis-dendro-default -p 6781:6780 -d redis:3.2.11-dendro-v0.3 -v ./data/redis-default:/data
-
 mkdir -p ./data/redis-social
-docker run --name redis-dendro-social -p 6782:6780 -d redis:3.2.11-dendro-v0.3 -v ./data/redis-default:/data
-
 mkdir -p ./data/redis-notifications
-docker run --name redis-dendro-notifications -p 6783:6780 -d redis:3.2.11-dendro-v0.3 -v ./data/redis-default:/data
+
+# Build images
+
+docker build \
+    -t virtuoso:7.2.2-dendro-v0.3 \
+    ../dockerfiles/virtuoso_with_criu_and_ontologies
+
+docker build \
+    -t elasticsearch:2.3.3-dendro-v0.3 \
+    ../dockerfiles/elasticsearch_with_criu
+
+docker build \
+    -t mysql:8.0.3-dendro-v0.3 \
+    ../dockerfiles/mysql_with_criu
+
+docker build \
+    -t mongo:3.4.10-dendro-v0.3 \
+    ../dockerfiles/mongo_with_criu
+
+docker build -t redis:3.2.11-dendro-v0.3 \
+    ../dockerfiles/redis_with_criu
+
+# Start containers
+
+docker run --name virtuoso-dendro \
+    -p 8890:8890 \
+    -p 1111:1111 \
+    -e SPARQL_UPDATE=true \
+    -v ./data/virtuoso:/data \
+    -d virtuoso:7.2.2-dendro-v0.3
+
+docker run --name elasticsearch-dendro \
+    -p 9200:9200 \
+    -v ./data/elasticsearch:/usr/share/elasticsearch \
+    -d elasticsearch:2.3.3-dendro-v0.3
+
+
+docker run --name mysql-dendro \
+    -p 3306:3306 \
+    -v ./data/mysql:/var/lib/mysql \
+    -d mysql:8.0.3-dendro-v0.3
+
+
+docker run --name mongo-dendro \
+    -p 27017:27017 \
+    -v ./data/mongo:/data/db \
+    -d mongo:3.4.10-dendro-v0.3
+
+
+docker run --name redis-dendro-default \
+    -p 6781:6780 \
+    -d redis:3.2.11-dendro-v0.3 \
+    -v ./data/redis-default:/data
+
+docker run --name redis-dendro-social \
+    -p 6782:6780 \
+    -d redis:3.2.11-dendro-v0.3 \
+    -v ./data/redis-default:/data
+
+docker run --name redis-dendro-notifications \
+    -p 6783:6780 \
+    -d redis:3.2.11-dendro-v0.3 \
+    -v ./data/redis-default:/data
