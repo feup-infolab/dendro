d2a12eadf667d21d386ebf5d754f8258539c91e7
João Rocha <joaorosilva@gmail.com>
2018-02-01 19:03:01 +0000

    Cleaned up the code

--- conf/scripts/deploy_docker_containers.sh
+++ conf/scripts/deploy_docker_containers.sh
@@ -36,38 +36,38 @@ docker run --name virtuoso-dendro \
     -p 8890:8890 \
     -p 1111:1111 \
     -e SPARQL_UPDATE=true \
-    -v ./data/virtuoso:/data \
+    -v data/virtuoso:/data \
     -d virtuoso:7.2.2-dendro-v0.3
 
 docker run --name elasticsearch-dendro \
     -p 9200:9200 \
-    -v ./data/elasticsearch:/usr/share/elasticsearch \
+    -v data/elasticsearch:/usr/share/elasticsearch \
     -d elasticsearch:2.3.3-dendro-v0.3
 
 
 docker run --name mysql-dendro \
     -p 3306:3306 \
-    -v ./data/mysql:/var/lib/mysql \
+    -v data/mysql:/var/lib/mysql \
     -d mysql:8.0.3-dendro-v0.3
 
 
 docker run --name mongo-dendro \
     -p 27017:27017 \
-    -v ./data/mongo:/data/db \
+    -v data/mongo:/data/db \
     -d mongo:3.4.10-dendro-v0.3
 
 
 docker run --name redis-dendro-default \
     -p 6781:6780 \
     -d redis:3.2.11-dendro-v0.3 \
-    -v ./data/redis-default:/data
+    -v data/redis-default:/data
 
 docker run --name redis-dendro-social \
     -p 6782:6780 \
     -d redis:3.2.11-dendro-v0.3 \
-    -v ./data/redis-default:/data
+    -v data/redis-default:/data
 
 docker run --name redis-dendro-notifications \
     -p 6783:6780 \
     -d redis:3.2.11-dendro-v0.3 \
-    -v ./data/redis-default:/data
+    -v data/redis-default:/data
