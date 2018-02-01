dad2bd2417675eb1149adb63aeee26bdf232dd00
João Rocha <joaorosilva@gmail.com>
2018-02-01 17:22:24 +0000

    Fixed paths to dockerfiles

--- conf/scripts/deploy_docker_containers.sh
+++ conf/scripts/deploy_docker_containers.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 #install and boot containers
-docker build -t virtuoso:7.2.2-dendro-v0.3 ./conf/dockerfiles/virtuoso_with_criu_and_ontologies
+docker build -t virtuoso:7.2.2-dendro-v0.3 ../dockerfiles/virtuoso_with_criu_and_ontologies
 docker run --name virtuoso-dendro -p 8890:8890 -p 1111:1111 -e SPARQL_UPDATE=true -d virtuoso:7.2.2-dendro-v0.3
 
-docker build -t elasticsearch:2.3.3-dendro-v0.3 ./conf/dockerfiles/elasticsearch_with_criu
+docker build -t elasticsearch:2.3.3-dendro-v0.3 ../dockerfiles/elasticsearch_with_criu
 docker run --name elasticsearch-dendro -p 9200:9200 -d elasticsearch:2.3.3-dendro-v0.3
 
-docker build -t mysql:8.0.3-dendro-v0.3 ./conf/dockerfiles/mysql_with_criu
+docker build -t mysql:8.0.3-dendro-v0.3 ../dockerfiles/mysql_with_criu
 docker run --name mysql-dendro -p 3306:3306 -d mysql:8.0.3-dendro-v0.3
 
-docker build -t mongo:3.4.10-dendro-v0.3 ./conf/dockerfiles/mongo_with_criu
+docker build -t mongo:3.4.10-dendro-v0.3 ../dockerfiles/mongo_with_criu
 docker run --name mongo-dendro -p 27017:27017 -d mongo:3.4.10-dendro-v0.3
 
-docker build -t redis:3.2.11-dendro-v0.3 ./conf/dockerfiles/redis_with_criu
+docker build -t redis:3.2.11-dendro-v0.3 ../dockerfiles/redis_with_criu
 
 docker run --name redis-dendro-default -p 6781:6780 -d redis:3.2.11-dendro-v0.3
 docker run --name redis-dendro-social -p 6782:6780 -d redis:3.2.11-dendro-v0.3
