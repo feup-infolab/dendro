99cf679c14536582917930e3d66530c11a0f1015
Nelson Pereira <nelsonpereira1991@gmail.com>
2018-02-20 14:30:19 +0000

    Created job to delete resources inside the temp folder that are older than two hours. Job runs hourly.

--- src/bootup/cron_jobs/delete_old_temp_folders.js
+++ src/bootup/cron_jobs/delete_old_temp_folders.js
@@ -1,29 +1,66 @@
 var CronJob = require('cron').CronJob;
 const Pathfinder = global.Pathfinder;
 const Logger = require(Pathfinder.absPathInSrcFolder("utils/logger.js")).Logger;
-const tmpLocation = Pathfinder.absPathInApp("temp");
+const tmpLocation = Pathfinder.absPathInApp("temp/");
 const exec = require("child_process").exec;
+const isNull = require(Pathfinder.absPathInSrcFolder("/utils/null.js")).isNull;
 
 const deleteOldTempFolders = function (app, callback)
 {
-    console.log("temp: " + tmpLocation);
-    const deleteTempFoldersOlderThen1Day = function (callback) {
-        //resources older than one hour
-        let result = null;
-        callback(null, result);
+    Logger.log("info", "Temp folder location is: " + tmpLocation);
+    const deleteTempFoldersOlderThen1Day = function (cb) {
+        //resources older than two hour
+        let resourcesToDelete = null;
+        //deletes resources older than two hours
+        exec("find " + tmpLocation + " -not -newermt '-7200 seconds' -mindepth 1", function(err, stdout, stderr)
+        {
+            if(!isNull(err))
+            {
+                const errorMsg = "Error finding old resources in the delete_old_temp_folders job: " + err;
+                cb(true, errorMsg);
+            }
+            else
+            {
+                resourcesToDelete = stdout;
+                exec("find " + tmpLocation + " -not -newermt '-7200 seconds' -mindepth 1 -delete", function(err, stdout, stderr)
+                {
+                    if(!isNull(err))
+                    {
+                        const errorMsg = "Error executing the delete_old_temp_folders job: " + err;
+                        cb(true, errorMsg);
+                    }
+                    else
+                    {
+                        const msg = "Executed the delete_old_temp_folders job successfully, deleted: " + resourcesToDelete;
+                        cb(null, msg);
+                    }
+                });
+            }
+        });
+        //find /Users/nelsonpereira/Desktop/infolaRepos/dendroRepo/dendro/temp/ -not -newermt '-7200 seconds' -mindepth 1 -delete
     };
     try {
-        let job = new CronJob('* * * * * *', function() {
+        //Every hour
+        let job = new CronJob("0 * * * *", function() {
             deleteTempFoldersOlderThen1Day(function (err, result) {
-                Logger.log("info", "Deleted: " + JSON.stringify(result));
+                if(!isNull(err))
+                {
+                    Logger.log("error", result);
+                }
+                else
+                {
+                    Logger.log("info", result);
+                }
             });
         }, null, true, 'America/Los_Angeles');
         job.start();
+        const jobMsg = "Hourly delete_old_temp_folders job started";
+        Logger.log("info", jobMsg);
         callback(null, null);
     } catch(ex) {
-        const errorMsg = "Invalid job pattern: " + ex.toString();
-        Logger.log("error", errorMsg);
-        callback(true, errorMsg);
+        const errMsg = "Invalid delete_old_temp_folders job pattern: " + ex.toString();
+        Logger.log("error", errMsg);
+        callback(true, errMsg);
     }
 };
 
