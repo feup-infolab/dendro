96634ca04d9dbe4cfa157ba086178320c71e7e49
Nelson Pereira <nelsonpereira1991@gmail.com>
2018-02-21 17:06:48 +0000

    changes done in the delete_old_temp_folder job:
     - Changed the value of the tmp folder location -> now uses the value specified in the config file
     - Job now calls the isSafePath before deleting any temp files

--- src/bootup/cron_jobs/delete_old_temp_folders.js
+++ src/bootup/cron_jobs/delete_old_temp_folders.js
@@ -1,7 +1,9 @@
 var CronJob = require('cron').CronJob;
 const Pathfinder = global.Pathfinder;
+const InformationElement = require(Pathfinder.absPathInSrcFolder("/models/directory_structure/information_element.js")).InformationElement;
 const Logger = require(Pathfinder.absPathInSrcFolder("utils/logger.js")).Logger;
-const tmpLocation = Pathfinder.absPathInApp("temp/");
+const Config = require(Pathfinder.absPathInSrcFolder("models/meta/config.js")).Config;
+const tmpLocation = Config.tempFilesDir;
 const exec = require("child_process").exec;
 const isNull = require(Pathfinder.absPathInSrcFolder("/utils/null.js")).isNull;
 const fs = require("fs");
@@ -16,29 +18,32 @@ const deleteOldTempFolders = function (app, callback)
         //find /Users/nelsonpereira/Desktop/infolaRepos/dendroRepo/dendro/temp/ -not -newermt '-7200 seconds' -mindepth 1 -delete
         //TODO look for a way to do this using nodejs code only -> so that it runs on machines other than macOs and Ubunto
         if (fs.existsSync(tmpLocation)) {
-            exec("find " + tmpLocation + " -not -newermt '-7200 seconds' -mindepth 1", function(err, stdout, stderr)
-            {
-                if(!isNull(err))
-                {
-                    const errorMsg = "Error finding old resources in the delete_old_temp_folders job: " + err;
-                    cb(true, errorMsg);
+            InformationElement.isSafePath(tmpLocation, function (err, isSafe) {
+                if (!err && isSafe) {
+                    exec("find " + tmpLocation + " -not -newermt '-7200 seconds' -mindepth 1", function (err, stdout, stderr) {
+                        if (!isNull(err)) {
+                            const errorMsg = "Error finding old resources in the delete_old_temp_folders job: " + err;
+                            cb(true, errorMsg);
+                        }
+                        else {
+                            resourcesToDelete = stdout;
+                            exec("find " + tmpLocation + " -not -newermt '-7200 seconds' -mindepth 1 -delete", function (err, stdout, stderr) {
+                                if (!isNull(err)) {
+                                    const errorMsg = "Error executing the delete_old_temp_folders job: " + err;
+                                    cb(true, errorMsg);
+                                }
+                                else {
+                                    const msg = "Executed the delete_old_temp_folders job successfully, deleted: " + resourcesToDelete;
+                                    cb(null, msg);
+                                }
+                            });
+                        }
+                    });
                 }
                 else
                 {
-                    resourcesToDelete = stdout;
-                    exec("find " + tmpLocation + " -not -newermt '-7200 seconds' -mindepth 1 -delete", function(err, stdout, stderr)
-                    {
-                        if(!isNull(err))
-                        {
-                            const errorMsg = "Error executing the delete_old_temp_folders job: " + err;
-                            cb(true, errorMsg);
-                        }
-                        else
-                        {
-                            const msg = "Executed the delete_old_temp_folders job successfully, deleted: " + resourcesToDelete;
-                            cb(null, msg);
-                        }
-                    });
+                    const errorMsg = "Error executing the delete_old_temp_folders job: " + "Location " + tmpLocation + " is not a safe path to delete";
+                    cb(true, errorMsg);
                 }
             });
         }
