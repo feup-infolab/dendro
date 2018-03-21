f26e0ebe5265f10acf79cca8d8316a6e06df75f6
Nelson Pereira <nelsonpereira1991@gmail.com>
2018-03-21 11:46:00 +0000

    Working on creating mysql stored procedures during dendro init time

--- src/bootup/init/init_mysql.js
+++ src/bootup/init/init_mysql.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const needle = require("needle");
+const async = require("async");
 
 const Pathfinder = global.Pathfinder;
 const Config = require(Pathfinder.absPathInSrcFolder("models/meta/config.js")).Config;
@@ -10,6 +11,116 @@ const initMySQL = function (app, callback)
 {
     Logger.log_boot_message("Setting up MySQL connection pool.");
     const mysql = require("mysql");
+    
+    const createStoredProcedures = function (pool, callback) {
+        const createGetProjectFavoriteDescriptorsProcedure = function (cb) {
+            const mysql = Config.getMySQLByID();
+            const dbName = Config.mySQLDBName;
+            const targetTable = Config.recommendation.getTargetTable();
+            const interactionsTable = dbName + "."+ targetTable;
+            const procedureName = "getProjectFavoriteDescriptors";
+            const queryString = "DROP PROCEDURE IF EXISTS " + procedureName + "; \n" +
+                "CREATE PROCEDURE " + procedureName + "(IN projectUri VARCHAR(255))" + " \n" +
+                "BEGIN \n" +
+                "SELECT DISTINCT favoritesInfo.executedOver, favoritesInfo.created, favoritesInfo.interactionType FROM " + interactionsTable + " AS favoritesInfo \n" +
+                "JOIN " + interactionsTable + " AS unfavoritesInfo \n" +
+                "ON favoritesInfo.interactionType in \n" +
+                "('favorite_descriptor_from_manual_list_for_project', 'favorite_descriptor_from_quick_list_for_project', 'unfavorite_descriptor_from_quick_list_for_project') \n" +
+                "AND favoritesInfo.projectUri = projectUri AND unfavoritesInfo.interactionType in ('favorite_descriptor_from_manual_list_for_project', 'favorite_descriptor_from_quick_list_for_project', 'unfavorite_descriptor_from_quick_list_for_project') \n" +
+                "AND unfavoritesInfo.projectUri = projectUri AND unfavoritesInfo.projectUri = favoritesInfo.projectUri AND favoritesInfo.executedOver = unfavoritesInfo.executedOver \n" +
+                "WHERE \n" +
+                "(favoritesInfo.created = (SELECT MAX(created) FROM " + interactionsTable + " WHERE projectUri = projectUri AND " + interactionsTable +".interactionType in ('favorite_descriptor_from_manual_list_for_project', 'favorite_descriptor_from_quick_list_for_project', 'unfavorite_descriptor_from_quick_list_for_project') AND favoritesInfo.interactionType in ('favorite_descriptor_from_manual_list_for_project', 'favorite_descriptor_from_quick_list_for_project') AND unfavoritesInfo.interactionType in ('favorite_descriptor_from_manual_list_for_project', 'favorite_descriptor_from_quick_list_for_project') AND unfavoritesInfo.executedOver = favoritesInfo.executedOver AND " + interactionsTable + ".executedOver = favoritesInfo.executedOver)) \n" +
+                "OR (favoritesInfo.created = (SELECT MAX(created) FROM " + interactionsTable + " WHERE projectUri = projectUri AND " + interactionsTable + ".interactionType in ('favorite_descriptor_from_manual_list_for_project', 'favorite_descriptor_from_quick_list_for_project', 'unfavorite_descriptor_from_quick_list_for_project') AND favoritesInfo.interactionType in ('favorite_descriptor_from_manual_list_for_project', 'favorite_descriptor_from_quick_list_for_project') AND unfavoritesInfo.interactionType = 'unfavorite_descriptor_from_quick_list_for_project' AND unfavoritesInfo.executedOver = favoritesInfo.executedOver AND " + interactionsTable + ".executedOver = favoritesInfo.executedOver AND  unfavoritesInfo.created < favoritesInfo.created)); \n" +
+                "END";
+
+            pool.getConnection(function (err, connection)
+            {
+                if (isNull(err))
+                {
+                    connection.query(queryString, function (err, rows, fields)
+                    {
+                        connection.release();
+                        if (isNull(err))
+                        {
+                            Logger.log("info", "Stored procedure: " + procedureName + " created");
+                            return cb(null, rows);
+                        }
+                        else
+                        {
+                            const errorMsg = "Error creating " + procedureName + " stored procedure in the MySQL database, error; " + JSON.stringify(err);
+                            Logger.log("error", errorMsg);
+                            return cb(true, errorMsg);
+                        }
+                    });
+                }
+                else
+                {
+                    const msg = "Unable to get MYSQL connection when looking for project: " + self.uri  + " favorite descriptors";
+                    Logger.log("error", msg);
+                    Logger.log("error", err.stack);
+                    return cb(1, msg);
+                }
+            });
+        };
+
+        const createGetProjectHiddenDescriptorsProcedure = function (cb) {
+            cb(null);
+        };
+
+        const createGetUserFavoriteDescriptorsProcedure = function (cb) {
+            cb(null);
+        };
+
+        const createGetUserHiddenDescriptorsProcedure = function (cb) {
+            cb(null);
+        };
+
+        async.waterfall([
+            function (cb)
+            {
+                createGetProjectFavoriteDescriptorsProcedure(function (err, info)
+                {
+                    if(!isNull(err))
+                    {
+                        Logger.log("error", JSON.stringify(info));
+                    }
+                    cb(err);
+                });
+            },
+            function (cb)
+            {
+                createGetProjectHiddenDescriptorsProcedure(function (err, info) {
+                    if(!isNull(err))
+                    {
+                        Logger.log("error", JSON.stringify(info));
+                    }
+                    cb(err);
+                })
+            },
+            function (cb)
+            {
+                createGetUserFavoriteDescriptorsProcedure(function (err, info) {
+                    if(!isNull(err))
+                    {
+                        Logger.log("error", JSON.stringify(info));
+                    }
+                    cb(err);
+                });
+            },
+            function (cb) {
+                createGetUserHiddenDescriptorsProcedure(function (err, info) {
+                    if(!isNull(err))
+                    {
+                        Logger.log("error", JSON.stringify(info));
+                    }
+                    cb(err);
+                });
+            }
+        ], function (err, results)
+        {
+            callback(err, results);
+        });
+    };
 
     const createDatabase = function (callback)
     {
@@ -80,7 +191,20 @@ const initMySQL = function (app, callback)
                             if (result.length > 0)
                             {
                                 Logger.log_boot_message("Interactions table " + tablename + " exists in the MySQL database.");
-                                poolOK(pool);
+                                createStoredProcedures(pool, function (err, info) {
+                                    if(isNull(err))
+                                    {
+                                        poolOK(pool);
+                                    }
+                                    else
+                                    {
+                                        let errorMessage = "Unable to create stored procedures in MySQL, error: " + JSON.stringify(err);
+                                        Logger.log("error", errorMessage);
+                                        return callback(errorMessage);
+                                    }
+                                });
+                                //old code bellow
+                                //poolOK(pool);
                             }
                             else
                             {
@@ -126,7 +250,20 @@ const initMySQL = function (app, callback)
                                             if (isNull(err))
                                             {
                                                 Logger.log_boot_message("Indexes on table  " + tablename + " succesfully created in the MySQL database.");
-                                                poolOK(pool);
+                                                createStoredProcedures(pool, function (err, info) {
+                                                    if(isNull(err))
+                                                    {
+                                                        poolOK(pool);
+                                                    }
+                                                    else
+                                                    {
+                                                        let errorMessage = "Unable to create stored procedures in MySQL, error: " + JSON.stringify(err);
+                                                        Logger.log("error", errorMessage);
+                                                        return callback(errorMessage);
+                                                    }
+                                                });
+                                                //old code bellow
+                                                //poolOK(pool);
                                             }
                                             else
                                             {
@@ -156,7 +293,6 @@ const initMySQL = function (app, callback)
                     {
                         return callback("Unable to create table " + tableForRecommendations + " in MySQL ");
                     }
-
                     poolOK(connection);
                 });
             }
