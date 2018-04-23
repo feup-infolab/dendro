705e918d16d135d46e87ca64f9cc8d1335a4016a
João Rocha da Silva <joaorosilva@gmail.com>
2018-04-23 20:31:06 +0100

    Changed node use commands in install.sh to remove version prefix.

--- conf/scripts/install.sh
+++ conf/scripts/install.sh
@@ -33,11 +33,11 @@ else
 
     source $HOME/.bash_profile
     nvm install "$NODE_VERSION"
-    nvm use "$NODE_VERSION"
+    nvm use --delete-prefix "$NODE_VERSION" --silent
 
     echo "Installing Node Version $NODE_VERSION during install script!!"
     nvm install $NODE_VERSION &&
-    nvm use $NODE_VERSION &&
+    nvm use --delete-prefix $NODE_VERSION --silent &&
     echo "loaded NVM."
 
     #update npm
