424d1af9caa543d0b2bae6b6e81efa98b33a7849
João Rocha <joaorosilva@gmail.com>
2018-07-18 14:48:26 +0100

    More tests passing

--- test/index.Test.dev.js
+++ test/index.Test.dev.js
@@ -16,62 +16,6 @@ global.Config = Config;
 
 global.tests = {};
 
-// SOCIAL DENDRO TESTS
-
-rlequire("dendro", "test/routes/notifications/all/routes.notifications.all.Test.js");
-
-rlequire("dendro", "test/routes/notifications/notification/routes.notifications.notification.Test.js");
-
-rlequire("dendro", "test/routes/notifications/notification/routes.notifications.notification[DELETE].Test.js");
-
-// END OF SOCIAL DENDRO TESTS
-
-// PUBLIC PROJECT FOLDER LEVEL ?VERSION
-rlequire("dendro", "test/routes/project/public_project/data/testFolder1/__version/routes.project.publicProject.data.testFolder1.__version.Test.js");
-rlequire("dendro", "test/routes/project/public_project/data/testFolder2/__version/routes.project.publicProject.data.testFolder2.__version.Test.js");
-
-// PRIVATE PROJECT FOLDER LEVEL ?VERSION
-rlequire("dendro", "test/routes/project/private_project/data/testFolder1/__version/routes.project.privateProject.data.testFolder1.__version.Test.js");
-rlequire("dendro", "test/routes/project/private_project/data/testFolder2/__version/routes.project.privateProject.data.testFolder2.__version.Test.js");
-
-// METADATA ONLY PROJECT FOLDER LEVEL ?VERSION
-rlequire("dendro", "test/routes/project/metadata_only_project/data/testFolder1/__version/routes.project.metadataonlyProject.data.testFolder1.__version.Test.js");
-rlequire("dendro", "test/routes/project/metadata_only_project/data/testFolder2/__version/routes.project.metadataonlyProject.data.testFolder2.__version.Test.js");
-
-// PUBLIC PROJECT FOLDER LEVEL ?CHANGE_LOG
-rlequire("dendro", "test/routes/project/public_project/data/testFolder1/__change_log/routes.project.publicProject.data.testFolder1.__change_log.Test.js");
-rlequire("dendro", "test/routes/project/public_project/data/testFolder2/__change_log/routes.project.publicProject.data.testFolder2.__change_log.Test.js");
-
-// PRIVATE PROJECT FOLDER LEVEL ?CHANGE_LOG
-rlequire("dendro", "test/routes/project/private_project/data/testFolder1/__change_log/routes.project.privateProject.data.testFolder1.__change_log.Test.js");
-rlequire("dendro", "test/routes/project/private_project/data/testFolder2/__change_log/routes.project.privateProject.data.testFolder2.__change_log.Test.js");
-
-// METADATA ONLY PROJECT FOLDER LEVEL ?CHANGE_LOG
-rlequire("dendro", "test/routes/project/metadata_only_project/data/testFolder1/__change_log/routes.project.metadataonlyProject.data.testFolder1.__change_log.Test.js");
-rlequire("dendro", "test/routes/project/metadata_only_project/data/testFolder2/__change_log/routes.project.metadataonlyProject.data.testFolder2.__change_log.Test.js");
-
-// PROJECT CHANGES PUBLIC PROJECT
-rlequire("dendro", "test/routes/project/public_project/__recent_changes/routes.project.publicProject.__recent_changes.Test.js");
-// PROJECT CHANGES PRIVATE PROJECT
-rlequire("dendro", "test/routes/project/private_project/__recent_changes/routes.project.privateProject.__recent_changes.Test.js");
-// PROJECT CHANGES METADADATA ONlY PROJECT
-rlequire("dendro", "test/routes/project/metadata_only_project/__recent_changes/routes.project.metadataonlyProject.__recent_changes.Test.js");
-
-// PUBLIC PROJECT FOLDER LEVEL RECENT CHANGES
-rlequire("dendro", "test/routes/project/public_project/data/testFolder1/__recent_changes/routes.project.publicProject.data.testFolder1.__recent_changes.Test.js");
-rlequire("dendro", "test/routes/project/public_project/data/testFolder2/__recent_changes/routes.project.publicProject.data.testFolder2.__recent_changes.Test.js");
-
-// PRIVATE PROJECT FOLDER LEVEL RECENT CHANGES
-rlequire("dendro", "test/routes/project/private_project/data/testFolder1/__recent_changes/routes.project.privateProject.data.testFolder1.__recent_changes.Test.js");
-rlequire("dendro", "test/routes/project/private_project/data/testFolder2/__recent_changes/routes.project.privateProject.data.testFolder2.__recent_changes.Test.js");
-
-// METADATA ONLY PROJECT FOLDER LEVEL RECENT CHANGES
-rlequire("dendro", "test/routes/project/metadata_only_project/data/testFolder1/__recent_changes/routes.project.metadataonlyProject.data.testFolder1.__recent_changes.Test.js");
-rlequire("dendro", "test/routes/project/metadata_only_project/data/testFolder2/__recent_changes/routes.project.metadataonlyProject.data.testFolder2.__recent_changes.Test.js");
-
-// Archived versions test
-rlequire("dendro", "test/routes/archived_resource/routes.archivedResource.Test.js");
-
 // Import projects tests
 rlequire("dendro", "test/routes/projects/import/route.projects.import.Test.js");
 
