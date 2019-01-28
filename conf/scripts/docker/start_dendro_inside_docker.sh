6cf135202bc2e1b56ad36f998a4c58acf6bfcbed
João Rocha da Silva <joaorosilva@gmail.com>
2019-01-28 18:06:05 +0000

    re-added waiting for boot...

--- conf/scripts/docker/start_dendro_inside_docker.sh
+++ conf/scripts/docker/start_dendro_inside_docker.sh
@@ -5,6 +5,53 @@ INSTALL_DIR="/tmp/dendro"
 RUNNING_DIR="$VOLUME_DIR/dendro"
 NODE_VERSION="$(cat $INSTALL_DIR/.nvmrc)"
 
+
+ELASTICSEARCH_HOST="elasticsearch-dendro"
+MONGO_HOST="mongodb-dendro"
+MARIADB_HOST="mariadb-dendro"
+VIRTUOSO_HOST="virtuoso-dendro"
+
+# starts containers with the volumes mounted
+function wait_for_server_to_boot_on_port()
+{
+    local ip=$1
+    local sentenceToFindInResponse=$2
+
+    if [[ $ip == "" ]]; then
+      ip="127.0.0.1"
+    fi
+    local port=$2
+    local attempts=0
+    local max_attempts=60
+
+    echo "Waiting for server on $ip:$port to boot up..."
+
+    response=$(curl -s $ip:$port)
+    echo $response
+
+	until $(curl --output /dev/null --silent --head --fail http://$ip:$port) || [[ $attempts > $max_attempts ]]; do
+        attempts=$((attempts+1))
+        echo "waiting... (${attempts}/${max_attempts})"
+        sleep 1;
+	done
+
+    if (( $attempts == $max_attempts ));
+    then
+        echo "Server on $ip:$port failed to start after $max_attempts"
+    elif (( $attempts < $max_attempts ));
+    then
+        echo "Server on $ip:$port started successfully at attempt (${attempts}/${max_attempts})"
+    fi
+}
+
+#wait for all servers to boot up before starting dendro
+wait_for_server_to_boot_on_port "$ELASTICSEARCH_HOST" 9200
+wait_for_server_to_boot_on_port "$MONGO_HOST" 27017
+wait_for_server_to_boot_on_port "$MARIADB_HOST" 3306
+wait_for_server_to_boot_on_port "$VIRTUOSO_HOST" 1111
+wait_for_server_to_boot_on_port "$VIRTUOSO_HOST" 8890
+
+
 # Switch to dendro user to start the app instead of using root
 HOME="/root"
 
