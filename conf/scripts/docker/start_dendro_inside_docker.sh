f9dfa1cfc9880256ef01dbd93ef297d2d50e22a6
João Rocha <joaorosilva@gmail.com>
2019-03-24 12:46:45 +0000

    More fixes

--- conf/scripts/docker/start_dendro_inside_docker.sh
+++ conf/scripts/docker/start_dendro_inside_docker.sh
@@ -8,9 +8,10 @@ function copy_source_to_destination()
   # diff -r -q "$source_dir" "$running_dir"
 
   echo "Copying files..."
-  shopt -s dotglob # for considering dot files (turn on dot files)
-  cp --recursive --no-target-directory "$source_dir" "$running_dir"
-  shopt -u dotglob # for don't considering dot files (turn off dot files)
+
+  diff -q "$source_dir" "$running_dir"
+
+  rsync --progress --human-readable --recursive "$source_dir"/ "$running_dir"/
 }
 
 # Switch to dendro user to start the app instead of using root
@@ -19,23 +20,23 @@ HOME="/root"
 echo "Dendro starting up at $DENDRO_RUNNING_DIR, installation dir $DENDRO_INSTALL_DIR and user $(whoami)"
 
 echo "Contents of install dir.................."
-ls -la $DENDRO_INSTALL_DIR
+ls -la "$DENDRO_INSTALL_DIR"
 echo "Contents of running dir.................."
-ls -la $DENDRO_RUNNING_DIR
+ls -la "$DENDRO_RUNNING_DIR"
 
 if [ -z "$(ls -A $DENDRO_RUNNING_DIR)" ]; then
 	echo "Dendro running dir is empty, so we assume this is the first bootup of the container."
 	echo "Copying all data from $DENDRO_INSTALL_DIR into $DENDRO_RUNNING_DIR..."
 else
 	echo "Dendro running directory ($DENDRO_RUNNING_DIR) is not empty, assuming it is already installed."
-  echo "Refreshing code on startup..."
+  echo "Synchronizing files"
 fi
 
 copy_source_to_destination "$DENDRO_INSTALL_DIR" "$DENDRO_RUNNING_DIR"
 
-# Change ownership
 echo "Contents of running dir $DENDRO_RUNNING_DIR"
 ls -la $DENDRO_RUNNING_DIR
+
 
 cd "$DENDRO_RUNNING_DIR" && echo "Switched to folder $(pwd) to start Dendro..." \
   || ( echo "Unable to find directory $DENDRO_RUNNING_DIR" && exit 1 )
@@ -53,14 +54,14 @@ node src/app.js --config="$DENDRO_ACTIVE_DEPLOYMENT_CONFIG"
 if [[ "$?" != "0" ]];
 then
   echo "There was an error starting dendro."
+  exit 1
   # echo "Showing the contents of $DENDRO_INSTALL_DIR and $(pwd)..."
   # printf "$DENDRO_INSTALL_DIR\n"
   # ls "$DENDRO_INSTALL_DIR"
   #
   # printf "$(pwd)\n"
   # ls "$(pwd)"
-
-  exit 1
+  #
   # echo "Trying to copy again all contents of install dir $DENDRO_DENDRO_INSTALL_DIR to running idr $DENDRO_RUNNING_DIR"
-  # copy_source_to_destination $DENDRO_INSTALL_DIR $DENDRO_RUNNING_DIR
+  # copy_source_to_destination "$DENDRO_INSTALL_DIR" "$DENDRO_RUNNING_DIR"
 fi
