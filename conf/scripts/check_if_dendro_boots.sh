a4310ebcdf1a9792cb406dbc9d9706999797d15c
João Rocha da Silva <joaorosilva@gmail.com>
2019-03-29 23:36:57 +0000

    Updated boot check script

--- conf/scripts/check_if_dendro_boots.sh
+++ conf/scripts/check_if_dendro_boots.sh
@@ -1,37 +1,50 @@
 #!/usr/bin/env bash
 
 # starts containers with the volumes mounted
-function wait_for_server_to_boot_on_port()
+function server_is_online()
 {
-    local ip=$1
+  local ip=$1
+  local port=$2
+  local text_to_find="$3"
+  local response
 
-    if [[ $ip == "" ]]; then
-      ip="127.0.0.1"
+  if [[ "$text_to_find" == "" ]];
+  then
+    echo "Waiting for server at $ip:$port to boot up..."
+    response=$(curl --silent --verbose "$ip:$port")
+    if [[ "$?" != "0" ]]; then
+      nc -z "$ip" "$port"
+      if [[ "$?" == "0" ]]; then
+        echo "Server IS ONLINE (checked via netcat)!"
+        return 0
+      else
+        echo "Server is not online. Response was $response"
+        return 1
+      fi
+    else
+      echo "Server IS ONLINE! Response was $response"
+      return 0
     fi
-    local port=$2
-    local attempts=0
-    local max_attempts=60
+  else
+    echo "Waiting for server at $ip:$port to boot up and respond with something like $text_to_find"
+    response=$(curl --silent --verbose --fail "$ip:$port")
 
-    echo "Waiting for server on $ip:$port to boot up..."
-
-    response=$(curl -s $ip:$port)
-    echo $response
-
-	until curl --output /dev/null --silent --head --fail http://$ip:$port || [[ $attempts > $max_attempts ]]; do
-        attempts=$((attempts+1))
-        echo "waiting... (${attempts}/${max_attempts})"
-        sleep 1;
-	done
-
-    if (( $attempts == $max_attempts ));
-    then
-        echo "Server on $ip:$port failed to start after $max_attempts"
-    elif (( $attempts < $max_attempts ));
-    then
-        echo "Server on $ip:$port started successfully at attempt (${attempts}/${max_attempts})"
+    if [[ "$?" != "0" ]]; then
+      if [[ "$response" == *$text_to_find* ]];
+      then
+        echo "Server IS ONLINE! Response was $response, which includes $text_to_find"
+        return 0
+      else
+        echo "Server is not online. Response was $response"
+        return 1
+      fi
+    else
+      echo "Server IS ONLINE! Response was $response"
+      return 0
     fi
+  fi
 }
 
 DENDRO_PORT=$1
 
-wait_for_server_to_boot_on_port "127.0.0.1" "$DENDRO_PORT"
+server_is_online "127.0.0.1" "$DENDRO_PORT"
