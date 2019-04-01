ae60a08be34bec9c7e6bdbd1c58c52a37bf2fb86
João Rocha da Silva <joaorosilva@gmail.com>
2019-04-01 01:03:05 +0100

    Create install_docker.sh

---
+++ conf/scripts/docker/install_docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Install dependencies
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+# Add Docker repo
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+
+# Install Docker
+sudo apt-get install -y docker-ce
+
+# Start Docker Daemon
+sudo service docker start
+
+# Add yourself to the Docker user group, otherwise docker will complain that
+# it does not know if the Docker Daemon is running
+sudo usermod -aG docker ${USER}
+
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+source ~/.bashrc
+docker-compose --version
+echo "Done!"
