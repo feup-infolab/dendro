4bbfb6e3ba6019a3804d7d5f1e03b788b78997ee
João Rocha da Silva <joaorosilva@gmail.com>
2019-05-13 17:40:18 +0100

    Removed 5 second parameter...

--- conf/scripts/build_automatico.sh
+++ conf/scripts/build_automatico.sh
@@ -3,15 +3,14 @@
 function runTest()
 {
   local NUM_THREADS=$1
-  #glances --export csv --export-csv-file "./glances_${NUM_THREADS}_thread.csv" --stdout-csv now,cpu,mem.used,load,diskio > /dev/null &
-  dstat -a -v --output ./glances_${NUM_THREADS}_thread.csv 1 5 > /dev/null &
+  dstat -a -v --output ./dstat_${NUM_THREADS}_thread.csv > /dev/null &
   DSTATS_PID=$!
   docker-mocha -f ./test/tests-structure-passing.json -t "${NUM_THREADS}" -c docker-compose-tests.yml -e dendro -p 3001 --config='docker_mocha' --stats-file="output_${NUM_THREADS}_thread.csv"
   kill -INT "$DSTATS_PID"
 }
 
 # docker-compose -f docker-compose-tests.yml pull
-# npm run docker-mocha-rebuild-dendro-image-nc
+npm run docker-mocha-rebuild-dendro-image-nc
 
 for i in 24 12 8 4 2 1
 do
