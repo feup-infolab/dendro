f0d03b3a786a0fec34ac2b17161d036fefa25704
Nuno Neto <nuno_mgl@hotmail.com>
2019-05-13 14:29:33 +0100

    updated scripts again

--- conf/scripts/build_automatico.sh
+++ conf/scripts/build_automatico.sh
@@ -5,7 +5,7 @@ function runTest()
   local NUM_THREADS=$1
   glances --export csv --export-csv-file "./glances_${NUM_THREADS}_thread.csv" --stdout-csv now,cpu,mem.used,load,diskio > /dev/null &
   GLANCES_PID=$!
-  npm run docker-mocha-bin -f ./test/tests-structure-passing.json -t "${NUM_THREADS}" -c docker-compose-tests.yml -e dendro -p 3001 --config='docker_mocha' --stats-file="output_${NUM_THREADS}_thread.csv"
+  docker-mocha -f ./test/tests-structure-passing.json -t "${NUM_THREADS}" -c docker-compose-tests.yml -e dendro -p 3001 --config='docker_mocha' --stats-file="output_${NUM_THREADS}_thread.csv"
   kill -INT "$GLANCES_PID"
 }
 
