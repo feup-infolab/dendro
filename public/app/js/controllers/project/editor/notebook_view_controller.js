3881294adc2b83dd1cc7811d85dbd65b83aecb44
BruunoMarques <Pandasarehere@gmail.com>
2019-06-03 16:13:39 +0100

    Adding controller for viewer

---
+++ public/app/js/controllers/project/editor/notebook_view_controller.js
@@ -0,0 +1,41 @@
+angular.module("dendroApp.controllers")
+/*
+    *  Notebook controller
+    */
+    .controller("notebookViewCtrl", function (
+        $scope,
+        $rootScope,
+        $http,
+        $filter,
+        $q,
+        $log,
+        $timeout,
+        $compile,
+        Upload,
+        focus,
+        preview,
+        $localStorage,
+        metadataService,
+        windowService,
+        cacheService,
+        filesService,
+        interactionsService,
+        ontologiesService,
+        storageService,
+        recommendationService,
+        usersService,
+        ContextMenuEvents,
+        notebookService
+    )
+    {
+        $scope.init = function ()
+        {
+
+        };
+
+       $scope.start_listener = function ()
+        {
+
+        };
+    });
+
