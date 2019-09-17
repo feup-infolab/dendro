7df2217ebe0d4253cdd470a00a9996422d30ea45
BruunoMarques <Pandasarehere@gmail.com>
2019-09-17 22:26:43 +0100

    FileWatcher not specific to notbook folder yet

--- src/controllers/notebooks.js
+++ src/controllers/notebooks.js
@@ -2,6 +2,8 @@ const rlequire = require("rlequire");
 const _ = require("underscore");
 const Notebook = rlequire("dendro", "src/models/directory_structure/notebook.js").Notebook;
 
+const chokidar = require('chokidar');
+
 module.exports.show = function (req, res)
 {
 };
@@ -18,5 +20,20 @@ module.exports.new = function (req, res)
     {
         res.redirect(`/notebook_runner/${newNotebook.id}`);
     });
+    fileWatcher();
 };
 
+fileWatcher = function () {
+
+    const watcher = chokidar.watch('../temp', {
+        ignored: /(^|[\/\\])\../, // ignore dotfiles
+        persistent: true
+    });
+
+    const log = console.log.bind(console);
+    watcher
+        .on('add', path => log(`File ${path} has been added`))
+        .on('change', path => log(`File ${path} has been changed`))
+        .on('unlink', path => log(`File ${path} has been removed`));
+
+};
